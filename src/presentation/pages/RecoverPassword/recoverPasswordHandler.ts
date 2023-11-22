ad48fb20a61db1e86822fa0fe54711c91f063e31
Reign-Beaux <samc920130@hotmail.com>
2023-11-21 21:27:23 -0600

    a

--- src/presentation/pages/RecoverPassword/recoverPasswordHandler.ts
+++ src/presentation/pages/RecoverPassword/recoverPasswordHandler.ts
@@ -1,16 +1,15 @@
 import { LoginModel } from "@/application/common/models";
-import { useAxios } from "@/application/libraries/axios/useAxios";
-import { useNavigate } from "react-router-dom";
-import { useFormSettings } from "./helpers";
 import { Login } from "@/application/common/statics";
+import { useAxios } from "@/application/libraries/axios/useAxios";
+import { useFormSettings } from "./helpers";
 
 export const useRecoverPasswordHandler = () => {
   const { post } = useAxios();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const sendMail = async (values: LoginModel) => {
     try {
-      const response = await post<string, LoginModel>(Login.RECOVER, values);
+      const response = await post<string, LoginModel>(Login.AUTHENTICATE, values);
       console.log(response);
     }
     catch (error: any) {
