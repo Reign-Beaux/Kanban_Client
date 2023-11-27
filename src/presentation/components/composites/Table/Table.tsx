86f45232d1a361362e4d882a1b211e8471cc5402
Reign-Beaux <samc920130@hotmail.com>
2023-11-26 20:30:33 -0600

    Se modificop la tabla

--- src/presentation/components/composites/Table/Table.tsx
+++ src/presentation/components/composites/Table/Table.tsx
@@ -1,5 +1,3 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import "./styles.css";
 import { Button } from "@/presentation/components/composites";
 import {
   IconAngleLeft,
@@ -7,18 +5,89 @@ import {
   IconAnglesLeft,
   IconAnglesRight,
 } from "@/presentation/components/elements";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import "./styles.css";
+import { useEffect, useState } from "react";
 
-interface TableProps<T> {
+export interface TableSettings<T> {
   data: T[];
-  columns: ColumnDef<T, any>[];
+  currentPage: number;
+  totalRecords: number;
 }
 
-export const Table = <T,>({ data, columns }: TableProps<T>) => {
+export const tableSettingsEmpty = <T,>() => {
+  return {
+    data: [] as T[],
+    currentPage: 0,
+    totalRecords: 0,
+  } as TableSettings<T>;
+};
+
+interface TableProps<T> {
+  settings: TableSettings<T>;
+  columns: ColumnDef<T, any>[];
+  getTableRecords: (page: number) => void;
+}
+
+enum Change {
+  FIRST,
+  PREVIOUS,
+  NEXT,
+  LAST,
+}
+
+export const Table = <T,>({
+  settings: { data, currentPage, totalRecords },
+  columns,
+  getTableRecords,
+}: TableProps<T>) => {
+  const pageSizes = [10, 20, 50];
+  const pageDefault = 10;
+  const [pageSize, setPageSize] = useState(pageDefault);
+  const [pageRange, setPageRange] = useState("0 - 0 de ");
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
+
+  const changePageSize = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    setPageSize(parseInt(event.target.value));
+
+  const calculatePageRange = () => {
+    let secondValue = currentPage * pageSize;
+    let firstValue = secondValue - pageSize + 1;
+    secondValue = Math.min(secondValue, totalRecords);
+    setPageRange(`${firstValue} - ${secondValue} de `);
+  };
+
+  const changePage = (value: Change) => {
+    debugger;
+    const lasPage = totalRecords / pageSize;
+    const pageMapping: Record<Change, number> = {
+      [Change.FIRST]: 1,
+      [Change.PREVIOUS]: Math.max(currentPage - 1, 1),
+      [Change.NEXT]: Math.min(currentPage + 1, Math.ceil(lasPage)),
+      [Change.LAST]: Math.ceil(lasPage),
+    };
+
+    const newPage = pageMapping[value];
+    getTableRecords(newPage);
+  };
+
+  useEffect(() => {
+    console.log("modificando currentPage");
+    calculatePageRange();
+  }, [currentPage]);
+
+  useEffect(() => {
+    changePage(Change.FIRST);
+  }, [pageSize]);
+
+  useEffect(() => {
+    console.log("entrando a table");
+  }, []);
 
   return (
     <>
@@ -68,24 +137,32 @@ export const Table = <T,>({ data, columns }: TableProps<T>) => {
       </div>
       <div className="paginator-container">
         <div style={{ width: "fit-content", margin: "5px" }}>
-          Filas por página{" "}
-          <select id="frutas" name="frutas" style={{width: "50px", textAlign: "end"}}>
-            <option value="manzana">10</option>
-            <option value="platano">20</option>
-            <option value="fresa">50</option>
+          <label htmlFor="page-sizes">Filas por página </label>
+          <select
+            id="page-sizes"
+            value={pageSize}
+            onChange={changePageSize}
+            style={{ width: "50px", textAlign: "end" }}>
+            {pageSizes.map((i) => (
+              <option key={i} value={i}>
+                {i}
+              </option>
+            ))}
           </select>
         </div>
-        <div style={{ width: "fit-content", margin: "5px" }}>1 - 10 de 50</div>
-        <Button isIcon style={{ margin: "5px" }}>
+        <div style={{ width: "fit-content", margin: "5px" }}>
+          {pageRange} {totalRecords}
+        </div>
+        <Button isIcon onClick={() => changePage(Change.FIRST)} style={{ margin: "5px" }}>
           <IconAnglesLeft />
         </Button>
-        <Button isIcon style={{ margin: "5px" }}>
+        <Button isIcon onClick={() => changePage(Change.PREVIOUS)} style={{ margin: "5px" }}>
           <IconAngleLeft />
         </Button>
-        <Button isIcon style={{ margin: "5px" }}>
+        <Button isIcon onClick={() => changePage(Change.NEXT)} style={{ margin: "5px" }}>
           <IconAngleRight />
         </Button>
-        <Button isIcon style={{ margin: "5px" }}>
+        <Button isIcon onClick={() => changePage(Change.LAST)} style={{ margin: "5px" }}>
           <IconAnglesRight />
         </Button>
       </div>
