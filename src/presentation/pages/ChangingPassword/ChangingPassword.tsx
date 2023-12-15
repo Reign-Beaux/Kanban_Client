dfdbf0da0b34dd4b4ecb2f7b4d5f59f947f5f4d1
Reign-Beaux <samc920130@hotmail.com>
2023-12-14 22:52:53 -0600

    se creó componente de chambio de contraseña le falta validación

--- src/presentation/pages/ChangingPassword/ChangingPassword.tsx
+++ src/presentation/pages/ChangingPassword/ChangingPassword.tsx
@@ -1,3 +1,31 @@
+import { OnlyString } from "@/application/common/models";
+import { Login, StatusResponse } from "@/application/common/statics";
+import { useAxios } from "@/application/libraries/axios/useAxios";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./styles.css";
+
 export const ChangingPassword = () => {
-  return <h1>ChangingPassword</h1>
-}
+  const navigate = useNavigate();
+  const { token } = useParams();
+  const { post } = useAxios();
+
+  const sendTokenRecoverPassword = async () => {
+    const response = await post<Response, OnlyString>(Login.RECOVER_PASSWORD_STEP_2, {
+      parameter: token!,
+    });
+    if (response.status === StatusResponse.OK) {
+      setTimeout(() => navigate("/login"), 2000);
+    }
+  };
+
+  useEffect(() => {
+    //sendTokenRecoverPassword();
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "15%" }}>
+      <span className="loader-changing-password">Generando una nueva contraseña, espere un momento.</span>
+    </div>
+  );
+};
